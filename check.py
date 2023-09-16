f7d8843cde55dbea253ef737b2b6f80b84165d00
3571303 <145234825+3571303@users.noreply.github.com>
2023-09-16 16:20:28 -0700

    update

--- check.py
+++ check.py
@@ -1,6 +1,5 @@
 # https://github.com/3571303/g4f-check
-# License: AGPL-3.0
-
+# License: AGPL-3.0-Star
 
 import sys
 import g4f
@@ -16,6 +15,10 @@ def process_provider(pname, results, fastest_provider):
             provider=getattr(getattr(getattr(sys.modules[__name__], "g4f"), "Provider"), pname),
             messages=[{"role": "user", "content": "Hello"}],
         )
+        if (response.strip() == ''):
+            raise Exception('Empty result')
+        if ('support@chatbase.co' in response):
+            raise Exception('ChatBase.co')
         end_time = time.time()
         time_taken = end_time - start_time
         results.append((pname, end_time - start_time))
@@ -29,7 +32,7 @@ def process_provider(pname, results, fastest_provider):
 providers = g4f.Provider.__all__
 
 results = []
-fastest_provider = ["", 99999]
+fastest_provider = ["NO WORKING PROVIDERS", 99999]
 threads = []
 for pname in providers:
     thread = threading.Thread(target=process_provider, args=(pname, results, fastest_provider))
